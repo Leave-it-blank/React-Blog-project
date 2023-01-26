8ea6d2a67e75886de169d339c4de74ef8ecad7c0
leaveitblank32 <leaveitblank32@gmail.com>
2023-01-27 01:24:18 +0530

    css changes

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,16 +1,23 @@
- import { useNavigate } from "react-router-dom";
-  function Modal ({children}){
-
-    const navigate = useNavigate();
-    function hideModal(){
-        navigate('..');
-    }
-    return <>
-        <div className=" absolute top-0 left-0 w-full h-screen bg-neutral-800  opacity-70"  onClick={hideModal}/>
-        <dialog open  className=" shadow-sm overflow-hidden p-3  bg-transparent ">
-           {children}
-        </dialog>
+import { useNavigate } from "react-router-dom";
+function Modal({ children }) {
+  const navigate = useNavigate();
+  function hideModal() {
+    navigate("..");
+  }
+  return (
+    <>
+      <div
+        className=" absolute top-0 left-0 w-full h-screen bg-neutral-800  opacity-70"
+        onClick={hideModal}
+      />
+      <dialog
+        open
+        className=" shadow-md overflow-hidden p-2 md:mt-10  bg-neutral-900 rounded-md  sm:w-2/3"
+      >
+        {children}
+      </dialog>
     </>
+  );
 }
 
 export default Modal;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,17 +1,28 @@
-
-function Post(props){
- 
-    return (
+import { Link } from "react-router-dom";
+function Post(props) {
+  //console.log(props);
+  return (
     <div className="bg-neutral-900 p-3 m-2 rounded-md">
-         <li >
-            <p className="bold capitalize text-2xl font-sans mb-2"> {props.title}</p>
-        <p className="text-sm pl-4 font-serif text-neutral-400">
-           by {props.author}
-        </p>
-        <p className="mt-5"></p>
-        <p className="text-neutral-200 px-4 py-1">{props.body}</p>
-    </li></div>)
-}
+      <li>
+        <Link to={props.id}>
+          <div className="flex flex-row justify-between align-middle">
+            <p className="bold capitalize text-2xl font-sans mx-3 py-2">
+              {props.title}
+            </p>
+            <p className="text-xs text-gray-400 pl-3">ID {props.id} </p>
+          </div>
+          <hr className="border-neutral-700 mx-3" />
+          <p className="text-sm my-2 px-4 mx-3 text-right font-serif text-neutral-400">
+            by <span className="text-white">{props.author}</span>
+          </p>
 
+          <p className="text-neutral-200 px-4 py-1 truncate ease-linear">
+            {props.body}
+          </p>
+        </Link>
+      </li>
+    </div>
+  );
+}
 
 export default Post;
